940ac3df494cb418c3404fa5208a1b053ffb06f6
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2023-03-27 00:32:25 +0200

    Add project files.

---
+++ NewsBlog/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using NewsBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewsBlog.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {
+        }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Page>? Pages { get; set; }
+
+    }
+}
