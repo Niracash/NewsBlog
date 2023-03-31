4cc7d2b9a3306cd07b9a6b5baf57af889e63ac61
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2023-03-31 11:05:21 +0200

    Admin

--- NewsBlog/Program.cs
+++ NewsBlog/Program.cs
@@ -40,8 +40,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
 
