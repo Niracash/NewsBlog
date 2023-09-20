bde7ca5526661764368cd07d1fd25dd332a46e0e
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2023-09-20 08:14:41 +0200

    Login added

--- NewsBlog/Areas/Admin/Controllers/UserController.cs
+++ NewsBlog/Areas/Admin/Controllers/UserController.cs
@@ -9,5 +9,10 @@ namespace NewsBlog.Areas.Admin.Controllers
         {
             return View();
         }
+        [HttpGet("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
