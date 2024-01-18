8f4c69523d0a245adbc88b8b99c12855b7043f12
Nirakash Sapkota <83993087+Niracash@users.noreply.github.com>
2024-01-18 12:07:47 +0100

    Home page frontend updated

--- NewsBlog/Controllers/HomeController.cs
+++ NewsBlog/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsBlog.Data;
 using NewsBlog.Models;
+using NewsBlog.ViewModels;
 using System.Diagnostics;
 
 namespace NewsBlog.Controllers
@@ -7,15 +10,28 @@ namespace NewsBlog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel();
+            var settings = _db.Settings!.ToList();
+
+            // Data from post
+            homeViewModel.Title = settings[0].Title;
+            homeViewModel.Description = settings[0].Description;
+            homeViewModel.ImageUrl = settings[0].ImageUrl;
+            homeViewModel.Posts = _db.Posts!.Include(x =>x.User).ToList();
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ NewsBlog/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using NewsBlog.Models;
+
+namespace NewsBlog.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
